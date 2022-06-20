32dc775db62cd3425df4ff3d9b66d943c642ac20
Peter <peter@uskee.org>
2022-06-20 17:26:19 +0800

    refine build script

--- run_bld.sh
+++ run_bld.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+# 1). svn checkout or git clone
+# 2). if svn, then decompress "submodules/openssl/fuzz/corpora.tgz" to "submodules/openssl/fuzz/corpora"
+# 3). if git, then switch and update to specific branch 
+#		git checkout dev.v1.9.1
+#		git submodule init
+#		git submodule update
 
-git checkout dev.v1.9.1
-git submodule init
-git submodule update
+ROOT=`pwd`
 
 path="dev19"
 rm -f build
@@ -11,13 +15,17 @@ ln -s $path build
 mkdir -p $path
 cd $path || exit 1
 
-cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_BUILD_TYPE=Release -DQUIC_ENABLE_LOGGING=OFF ..
 make -j4
 
-cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DQUIC_ENABLE_LOGGING=OFF ..
 make -j4
 
 rm -f openssl
 ln -s _deps/opensslquic-build/openssl
+
+cd ../../
+rm -f openssl_mms
+ln -s $ROOT/build/_deps/opensslquic-build/openssl openssl_mms
 
 exit 0
